4bab1c04f6ba443da79d3a7dbdb2d6c99e47c325
Aaron F Stanton <afstanton@gmail.com>
2025-10-06 14:44:36 -0400

    Zeitwerk

--- lib/docker_mcp.rb
+++ lib/docker_mcp.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+require 'zeitwerk'
+
+loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
+loader.setup # ready!
+
 require_relative 'docker_mcp/version'
 
 module DockerMcp
